5ea70055de47e50ac317f34a84684b318020c253
Kriti <73518229+kriti-agrawal-52@users.noreply.github.com>
2022-02-21 22:14:54 +0530

    Added finetuned models

--- src/services/GptAPI.ts
+++ src/services/GptAPI.ts
@@ -10,25 +10,48 @@ export interface ChoiceResult {
 
 class GptAPI {
     static generateCompletions(prompt: string | Array<string>, completionParams: CompletionParameters,
-                               n: number = 1): AxiosPromise {
-        return axios({
-            method: "POST",
-            url: `https://api.openai.com/v1/engines/${completionParams.engine}/completions`,
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${completionParams.apiKey}`,
-            },
-            data: {
-                "prompt": prompt,
-                "n": n,
-                "max_tokens": completionParams.maxTokens,
-                "temperature": completionParams.temperature,
-                "stop": completionParams.stop,
-                "top_p": completionParams.topP,
-                "presence_penalty": completionParams.presencePenalty,
-                "frequency_penalty": completionParams.frequencyPenalty
-            }
-        });
+        n: number = 1): AxiosPromise {
+        if(completionParams.engine.includes("writesonic")){
+            return axios({
+                method: "POST",
+                url: "https://api.openai.com/v1/completions",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${completionParams.apiKey}`,
+                },
+                data: {
+                    "prompt": prompt,
+                    "n": n,
+                    "max_tokens": completionParams.maxTokens,
+                    "temperature": completionParams.temperature,
+                    "stop": completionParams.stop,
+                    "top_p": completionParams.topP,
+                    "presence_penalty": completionParams.presencePenalty,
+                    "frequency_penalty": completionParams.frequencyPenalty,
+                    "model": completionParams.engine
+                }
+            });
+        }
+        else{
+            return axios({
+                method: "POST",
+                url: `https://api.openai.com/v1/engines/${completionParams.engine}/completions`,
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${completionParams.apiKey}`,
+                },
+                data: {
+                    "prompt": prompt,
+                    "n": n,
+                    "max_tokens": completionParams.maxTokens,
+                    "temperature": completionParams.temperature,
+                    "stop": completionParams.stop,
+                    "top_p": completionParams.topP,
+                    "presence_penalty": completionParams.presencePenalty,
+                    "frequency_penalty": completionParams.frequencyPenalty
+                }
+            });
+        }
     }
 }
 
